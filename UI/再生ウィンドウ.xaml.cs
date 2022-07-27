a7fb8a7d4663cfac9a813f55540d541aa96a93c6
Kurumi4019 <Kurumi4019@N1565CAW-E3>
2022-07-27 22:52:14 +0900

    なにしたかわすれた

--- UI/再生ウィンドウ.xaml.cs
+++ UI/再生ウィンドウ.xaml.cs
@@ -6,18 +6,19 @@ namespace mp4Utl.NewFolder
 /// <summary>
 /// PreviewWindow.xaml の相互作用ロジック
 /// </summary>
-{ public partial class PreviewWindow : Window
 {
-    public PreviewWindow()
+    public partial class PreviewWindow : Window
     {
-        InitializeComponent();
-    }
-    //　MenuItem_Clickは実装順にやってますので視認性ちょっと悪いかもしれません。
-    //　ご了承下さい。
+        public PreviewWindow()
+        {
+            InitializeComponent();
+        }
 
-    private void MenuItem_Click_Exit(object sender, RoutedEventArgs e)
-    //｢ファイル｣→｢ソフトウェアの終了｣をクリックしたときの動作
-    {
+        //　MenuItem_Clickは実装順にやってますので視認性ちょっと悪いかもしれません。
+        //　ご了承下さい。
+
+        private void MenuItem_Click_Exit(object sender, RoutedEventArgs e)
+        //｢ファイル｣→｢ソフトウェアの終了｣をクリックしたときの動作
         {
             MessageBoxResult Result = MessageBox.Show("ソフトウェアを終了しますか？", "確認", MessageBoxButton.YesNo, MessageBoxImage.Question);
 
@@ -30,30 +31,31 @@ namespace mp4Utl.NewFolder
             }
         }
 
-    }
-    private void MenuItem_Click_Version(object sender, RoutedEventArgs e) //｢ヘルプ｣→｢バージョン情報｣をクリックしたときの動作
-    {
-        MessageBox.Show("バージョン:α12\r\n作者:くるみ白羽\r\n\r\nこのソフトウェアは [GNU Lesser General Public License v3.1] ライセンスに基づき配布されています｡たぶんね");
-    }
-
-    private void MenuItem_Click_GitHub(object sender, RoutedEventArgs e) //｢ヘルプ｣→｢GitHubへのリンク｣をクリックしたときの動作
-    {
-        var startInfo = new System.Diagnostics.ProcessStartInfo("https://github.com/Kurumi4019/mp4Utl");
-        startInfo.UseShellExecute = true;
-        System.Diagnostics.Process.Start(startInfo);
-    }
-
-    private void MenuItem_Click_ItemProperty(object sender, RoutedEventArgs e) //｢表示｣→｢アイテムプロパティの表示｣をクリックしたときの動作
-    {
+        private void MenuItem_Click_Version(object sender, RoutedEventArgs e) //｢ヘルプ｣→｢バージョン情報｣をクリックしたときの動作
         {
-            var win = new Video();
-            win.Show();
+            MessageBox.Show("バージョン:α12\r\n作者:くるみ白羽\r\n\r\nこのソフトウェアは [GNU Lesser General Public License v3.1] ライセンスに基づき配布されています｡たぶんね");
+        }
+
+        private void MenuItem_Click_GitHub(object sender, RoutedEventArgs e) //｢ヘルプ｣→｢GitHubへのリンク｣をクリックしたときの動作
+        {
+            var startInfo = new System.Diagnostics.ProcessStartInfo("https://github.com/Kurumi4019/mp4Utl")
+            {
+                UseShellExecute = true
+            };
+            System.Diagnostics.Process.Start(startInfo);
+        }
+
+        private void MenuItem_Click_ItemProperty(object sender, RoutedEventArgs e) //｢表示｣→｢アイテムプロパティの表示｣をクリックしたときの動作
+        {
+            {
+                var win = new Video();
+                win.Show();
+            }
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) //シークバー
+        {
+
         }
     }
-
-    private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) //シークバー
-    {
-
-    }
-}
 }
